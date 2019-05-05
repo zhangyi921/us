89bd2e3a0d8d16f7b459a509e8ea211d5c2fa33e
Yi Zhang <yi16@ualberta.ca>
2019-05-04 23:22:32 -0600

    nav bar

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import logo from './logo.svg';
 import './App.css';
 import 'semantic-ui-css/semantic.min.css'
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
-import { Menu, Segment, Transition } from 'semantic-ui-react'
+import { Menu, Segment, Transition, Visibility, Button } from 'semantic-ui-react'
 import Zoom from 'react-reveal/Zoom';
 
 // this is my couple app demo
 // packages used: sematic ui, react reaveal
+// npm run predeploy
+// npm run deploy
 function Index() {
   return (
     <div>
@@ -43,26 +45,36 @@ class App extends React.Component {
   constructor(props){
     super(props);
     this.state = { activeItem: 'home', visible: false };
+    this.contextRef = React.createRef();
   }
   handleItemClick = (e, { name }) => this.setState({ activeItem: name })
-  listenToScroll = ()=>{
-    if (document.body.scrollTop > 660) {
-      this.setState({visible: true})
-    }
-    else{
-      this.setState({visible: false})
+  // listenToScroll = ()=>{
+  //   if (document.body.scrollTop > 660) {
+  //     this.setState({visible: true})
+  //   }
+  //   else{
+  //     this.setState({visible: false})
 
-    }
+  //   }
+  // }
+  // componentDidMount() {
+  //   window.addEventListener('scroll', this.listenToScroll)
+  // }
+  
+  showMenuBar = ()=>{
+    this.setState({visible: true})
   }
-  componentDidMount() {
-    window.addEventListener('scroll', this.listenToScroll)
+  hideMenuBar = ()=>{
+    this.setState({visible: false})
   }
+
   render(){
     const { activeItem } = this.state;
     return (
       <div className="App">
-        <Segment inverted style={{margin:0, background: "#a0d2ff", }} className="top-menu-bar">
-          <Menu inverted pointing secondary style={{border:"#a0d2ff"}}>
+        <Segment inverted style={{margin:0, background: "#a0d2ff", padding: 0}} className="top-menu-bar">
+          <Menu inverted pointing secondary style={{border:"black", background: "#a0d2ff", padding: 2}}  
+            size={this.state.visible ? "small": "huge"} fixed={this.state.visible ? 'top' : null}>
             <Menu.Item name='home' active={activeItem === 'home'} onClick={this.handleItemClick} style={{background: "#a0d2ff"}} color={"pink"}/>
             <Menu.Item
               name='messages'
@@ -78,8 +90,25 @@ class App extends React.Component {
               style={{background: "#a0d2ff"}}
               color={"pink"}
             />
+            <Menu.Item
+              name='login'
+              active={activeItem === 'login'}
+              onClick={this.handleItemClick}
+              style={{background: "#a0d2ff", border:"none"}}
+              color={"pink"}
+              position='right'
+            >
+              <Button as='a' inverted={!this.state.visible} color={this.state.visible ? "pink": "white"}>
+                Log in
+              </Button>
+              <Button as='a' inverted={!this.state.visible} primary={this.state.visible} style={{ marginLeft: '0.5em' }}>
+                Sign Up
+              </Button>
+            </Menu.Item>
           </Menu>
         </Segment>
+        
+        <Visibility context = {this.contextRef} onOnScreen={this.hideMenuBar} once={false}></Visibility>
         <header className="App-header">
           
           <Router>
@@ -103,10 +132,14 @@ class App extends React.Component {
             <Route path="/users/" component={Users} />
           </div>
         </Router>
+        <Visibility context = {this.contextRef} onBottomPassed = {this.showMenuBar} once={false}>
+
+        </Visibility>
         </header>
-        <Transition visible={this.state.visible} animation='scale' duration={500}>
+        <h1>test</h1>
+        {/* <Transition visible={this.state.visible} animation='scale' duration={500}>
           <Segment inverted id = 'menubar'>
-            <Menu color={"pink"} widths={3}>
+            <Menu color={"pink"} widths={4}  size='large'>
               <Menu.Item name='home' active={activeItem === 'home'} onClick={this.handleItemClick} />
               <Menu.Item
                 name='messages'
@@ -118,9 +151,14 @@ class App extends React.Component {
                 active={activeItem === 'friends'}
                 onClick={this.handleItemClick}
               />
+              <Menu.Item
+              name='login'
+              active={activeItem === 'login'}
+              onClick={this.handleItemClick}
+            />
             </Menu>
           </Segment>
-        </Transition>
+        </Transition> */}
         
       </div>
     );
